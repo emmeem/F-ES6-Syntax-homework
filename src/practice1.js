d34118d02dcb675b7982758f867dafc1bf3d5869
emmeem <1124782639@qq.com>
2020-09-11 00:12:35 +0800

    complete p1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = ({data, column}) => {
+  const keys = column.map(obj => obj.name);
+  const obj = []
+  data.map(info => {
+      const values = {};
+      info.map((item,index) => { 
+        values[keys[index]] = item})
+        obj.push(values)
+        })
+   return obj;
+}
+      
 export { parseData };
