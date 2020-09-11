70fcf19521cd6309cfcba29a4528462949c88847
emmeem <1124782639@qq.com>
2020-09-11 09:47:38 +0800

    fix practice2

--- src/practice1.js
+++ src/practice1.js
@@ -2,12 +2,11 @@ const parseData = ({data, column}) => {
   const keys = column.map(obj => obj.name);
   const obj = []
   data.map(info => {
-      const values = {};
-      info.map((item,index) => { 
-        values[keys[index]] = item})
-        obj.push(values)
-        })
-   return obj;
+    const values = {};
+    info.map((item,index) => { 
+      values[keys[index]] = item})
+      obj.push(values)})
+  return obj;
 }
       
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = (items, sections) => {
+    const contents = sections.map(obj => obj.content);
+    const position = sections.map(obj => obj.index);
+
+    contents.map((c,index) => {
+       items.splice(position[index]+index,0,c);
+      }
+    )
+    return items;
+}
+
 export { inject };
