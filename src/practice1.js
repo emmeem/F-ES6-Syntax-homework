c5c4b25b31b48cf644ceab8a07e99bd54f835696
kevindongzg <dongkevin94@gmail.com>
2020-09-22 00:31:25 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,5 +1,7 @@
 const parseData = ({data, column}) => {
+  // TODO keys 没必要
   const keys = column.map(obj => obj.name);
+  // TODO 使用map做数组与数组的转化，不用声明obj，查查map的使用
   const obj = []
   data.map(info => {
     const values = {};
--- src/practice2.js
+++ src/practice2.js
@@ -1,7 +1,10 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO contents与position 没有太大必要
     const contents = sections.map(obj => obj.content);
     const position = sections.map(obj => obj.index);
 
+    // TODO 不是map的使用场景
     contents.map((c,index) => {
        items.splice(position[index]+index,0,c);
       }
